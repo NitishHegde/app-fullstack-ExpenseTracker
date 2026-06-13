a66d880865c4107d8008696db00145e5870bd832
nitish_hegde <nitishhegde239@gmail.com>
2026-06-13 22:27:10 +0530

    frontEnd codefirst commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboardpage from './pages/Dashboardpage';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+const clearAuth = () =>{
+  try {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    sessionStorage.removeItem("user");
+    sessionStorage.removeItem("token");
+  } catch (error) {
+    console.log("clearAuth error: ",error);
+  }
+  setUser(null);
+  setToken(null);
+}
+  const handleLogout = () => {
+    clearAuth();
+    navigate("/login");
+  };
+
   return (
-    <div>  
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path='/' element={<Dashboardpage/>}/>
+        </Route>
+      </Routes>
+    </>
   )
 };
 
